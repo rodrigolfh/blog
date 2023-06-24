3746a2f6634b2d9f63529f084de7840fc47188ab
Anibal <anibalmreinoso@gmail.com>
2023-06-23 21:36:53 -0300

    creacion de modelos y vista

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentario de {self.author.username} en {self.post.title}"
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post, Comment
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(
+        request,
+        'blog/post_list.html',
+        {'posts':posts}
+    )
