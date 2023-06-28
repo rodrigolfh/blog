284f6f7f2e9c8d4601459ef0a40f7c02cadbcbb1
Anibal <anibalmreinoso@gmail.com>
2023-06-27 21:39:20 -0300

    jugando con el ORM

--- src/blog/models.py
+++ src/blog/models.py
@@ -4,10 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    TZ = [
+        ("EU", "EUROPE"),
+        ("AS", "ASIA"),
+        ("US", "UNITED STATES"),
+    ]
+    title = models.CharField(verbose_name="Titulo del Posteo", max_length=100)
+    content = models.TextField(verbose_name="Texto del Posteo", null=True, blank=True, default="Texto default")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor del Posteo")
+    created_at = models.DateTimeField(auto_now=True, verbose_name="Horario del posteo")
+    tzone = models.CharField(choices=TZ, verbose_name="Zona horaria")
 
     def __str__(self):
         return self.title
