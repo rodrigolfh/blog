b3e1b7f3913fc95644bcab1c1088974df0de0674
Anibal <anibalmreinoso@gmail.com>
2023-06-30 21:30:42 -0300

    filtro de posteos

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,7 +4,14 @@ from .models import Post, Comment
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
+
+    filtro = request.GET.get('filtro')
+
+    if filtro:
+        posts = Post.objects.filter(title__icontains=filtro)
+    else:
+        posts = Post.objects.all()
+
     return render(
         request,
         'blog/post_list.html',
