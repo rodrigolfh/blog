bfe4971423c03924c24cd311d15f7461a1566ff6
Anibal <anibalmreinoso@gmail.com>
2023-07-05 21:30:17 -0300

    modificacion de posteos

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('post_list/', views.post_list, name='post_list'),
+    path('crear_posteo/', views.crear_posteo, name='crear_posteo'),
+    path('modificar_posteo/<id>/', views.modificar_posteo, name='modificar_posteo'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Comment
+from .forms import PostForm
 
 # Create your views here.
 
@@ -17,3 +18,36 @@ def post_list(request):
         'blog/post_list.html',
         {'posts':posts}
     )
+
+def crear_posteo(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            author = form.cleaned_data["author"]
+            tzone = form.cleaned_data["tzone"]
+            post = Post.objects.create(
+                title=title,
+                content=content,
+                author=author,
+                tzone=tzone
+            )
+            return redirect("post_list")
+        else:
+            return render(request, 'blog/crear_posteo.html', {'form':form})
+    
+    return render(request, 'blog/crear_posteo.html', {'form':form})
+
+
+def modificar_posteo(request, id):
+    post = Post.objects.get(pk=id)
+    form = PostForm(instance=post)
+    if request.method =="POST":
+        form = PostForm(request.POST, instance=post)
+        form.save()
+        return redirect('post_list')
+    else:
+        return render(request, 'blog/modificar_posteo.html', {'form':form})
+    
